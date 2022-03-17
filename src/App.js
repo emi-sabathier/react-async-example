44f7dc5e1da13b3d3b987abaaecf7f54a15d774e
emi-sabathier <sabathier.emilie@gmail.com>
2022-03-17 20:03:53 +0100

    explications

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import {useEffect} from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState({}); // on va recevoir de l'API une promesse contenant un objet avec les datas
+
+  // async: c'est une fonction asynchrone, qui n'empêche pas tout le code de l'app de s'éxecuter
+  const fetchData = async () => {
+    try {
+      /* await: await indique qu'on renvoie une promesse. On "attend" le résultat de l'API github
+      Si l'api est down, ou autre, on passe dans le catch
+      */
+      const data = await axios.get('https://api.github.com/search/users?q=emi-sabathier');
+      setData(data.data);
+      console.log('Voilà les datas renvoyées: ', data)
+      return data;
+    } catch (error) {
+      /* Une erreur sera renvoyée ici s'il y en a une
+      Remplace https://api.github.com/search/users?q=emi-sabathier par https://api.github.com/search/urlquiexistepas dans le try
+       */
+      console.log("Détails de l'erreur venant de l'API: ", error)
+    }
+  }
+
+  /* useEffect se lance après le premier rendu (<div>...</div>)
+    Quand on fait appel à une API, on met la fonction qui appelle l'api dans un useEffect (ou componentDidMount() quand on est en react class)
+   */
+  useEffect(()=>{
+      fetchData();
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>ouvre la console</p>
+      <p>Username: {data.items[0].login}</p>
+      <p>Url: {data.items[0].url}</p>
     </div>
   );
 }
